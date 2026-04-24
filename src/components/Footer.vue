46b9064fed8603503878a24d8d8ae3771d9856ad
yanaguiii <lucasyukicod@hotmail.com>
2026-04-24 12:36:34 -0300

    fix: whatsapp

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@
     <div class="bp-footer__bot">
       <span>© Legacy Blackpink Cover 2026</span>
       <div class="bp-footer__links">
-        <a href="https://wa.me/5511957876548?text=Ol%C3%A1%21%20Gostaria%20de%20agendar%20um%20show%20do%20BLACKPINK." target="_blank" rel="noopener">whatsapp</a>
+        <a href="https://wa.me/5521966997227?text=Ol%C3%A1%21%20Gostaria%20de%20agendar%20um%20show%20do%20BLACKPINK." target="_blank" rel="noopener">whatsapp</a>
         <a href="https://www.instagram.com/legacyblackpink.cover/" target="_blank" rel="noopener">instagram</a>
         <a href="#contato">contato</a>
       </div>
